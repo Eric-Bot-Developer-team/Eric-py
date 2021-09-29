de3c85b6ad0e0fe5ad2a23c6bc462ee04ac05b8c
renevds <rene.v.d.s.9@gmail.com>
2021-09-29 15:12:10 +0200

    Fix

--- modules/game/discord_layer/game.py
+++ modules/game/discord_layer/game.py
@@ -2,20 +2,20 @@ import discord
 from discord.ext import commands, tasks
 from modules.game.logic_layer.connect4_logic import Connect4Logic, connect4games
 from time import time
-from easy_embed import simple_message
 
 
 class Game(commands.Cog):
 
     def __init__(self, bot: discord.Client):
         self.bot = bot
+        self.easy_embed = self.bot.easy_embed
         self.timeout.start()
 
     @commands.command(name='conn')
     async def connect4(self, ctx, move=None):
         message = await Connect4Logic.connect4logic(ctx.author, ctx.channel, move)
         if message is not None:
-            await simple_message(message, ctx)
+            await self.easy_embed.simple_message(message, ctx)
 
     @tasks.loop(seconds=5.0)
     async def timeout(self):
