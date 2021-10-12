b47d688ccc5708c93c09b3a5706e3f6c64d41e7b
renevds <rene.v.d.s.9@gmail.com>
2021-10-12 14:41:33 +0200

    Add E meme

--- modules/dank/dank.py
+++ modules/dank/dank.py
@@ -2,6 +2,7 @@ import json
 import random
 from urllib.parse import quote_plus
 
+import praw
 import requests
 from discord.ext import commands
 
@@ -9,6 +10,9 @@ from discord.ext import commands
 class Dank(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.reddit = praw.Reddit(client_id='vyMX9N2zNJds-GgIDktADg',
+                             client_secret='R5IQUYXKssyWfvFrzG9jVuVfkTGMMA',
+                             user_agent='windows:com.renevds.eric:1.0.0 (by u/renevds)')
 
     # for fun
     @commands.command(name='pp')
@@ -25,6 +29,15 @@ class Dank(commands.Cog):
         doge = json.loads(requests.get(API + quote_plus(' '.join(args))).text)['contents']['translated']
         await ctx.message.delete()
         await ctx.send(f'**{ctx.message.author.name}**: {doge}')
+
+    @commands.command()
+    async def meme(self, ctx):
+        memes_submissions = self.reddit.subreddit('EdgyMemestwo').hot()
+        post_to_pick = random.randint(1, 50)
+        for i in range(0, post_to_pick):
+            submission = next(x for x in memes_submissions if not x.stickied)
+
+        await ctx.send(submission.url)
 
 
 def setup(bot):
